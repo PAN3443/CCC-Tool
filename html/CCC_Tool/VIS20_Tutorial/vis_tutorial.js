0d213d8a28a8f94dc884d3e79090f8ff40919afa
PANeverland <Pascal.Nardini@gmx.de>
2020-10-12 12:31:11 +0200

    Start the VIS2020 tutorial

---
+++ html/CCC_Tool/VIS20_Tutorial/vis_tutorial.js
@@ -0,0 +1,61 @@
+var currentPos=0;
+var pageProcess=[];
+var numPages =7;
+
+window.onload = function() {
+    currentPos=0;
+    pageProcess=[];
+}
+
+function nextPage(){
+    pageProcess.push(currentPos);
+    stylePages(currentPos+1);
+}
+
+function prevPage() {
+    var previousePage = pageProcess.pop();
+    if(isNaN(previousePage))
+        return;
+    stylePages(previousePage);
+} 
+
+function goToPage(pageID){
+    pageProcess.push(currentPos);
+    stylePages(pageID);
+}
+
+function stylePages(pageID){
+
+    if(pageID==currentPos)
+        return;
+
+    ////// Style Pages /////////
+    /*for (let index = 0; index < numPages; index++) {
+        
+        switch (true) {
+            case (index==currentPos):
+                document.getElementById("id_page_"+index).style.left="0vw";
+            break;
+            case (index<currentPos):
+                document.getElementById("id_page_"+index).style.left="-100vw";
+            break;
+            default:
+                document.getElementById("id_page_"+index).style.left="100vw";
+        }
+
+    }
+
+    console.log(document.getElementById("id_page_"+pageID).style.left);*/
+
+    ////// Do Sliding /////////
+    document.getElementById("id_page_"+pageID).style.left="0vw";
+
+    if(pageID<currentPos)
+        document.getElementById("id_page_"+currentPos).style.left="100vw";
+    else
+        document.getElementById("id_page_"+currentPos).style.left="-100vw";
+
+    currentPos=pageID;
+
+}
+
